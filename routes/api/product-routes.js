c823acf4a408c016355b6b20641482ef3f3f9882
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-07-29 10:20:58 -0400

    added product delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -107,6 +107,16 @@ router.put("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({ where: { id: req.params.id } });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product with that ID!" });
+    }
+    res.status(200).json(ProductData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
